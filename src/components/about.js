411865a1788ec6a207ba4ecf3f7d397cd998db53
Ben <benjaminkhlau@gmail.com>
2023-04-02 14:25:50 -0500

    about page work in progress

--- src/components/about.js
+++ src/components/about.js
@@ -1,11 +1,25 @@
-
+import kiwi from "../pics/about.jpg"
 
 
 function About(){
 
     return(
         <div id="about">
-        About section is work in progress!! Styling coming soon!
+            <h1>About</h1>
+            <div className="about">Benjamin is a Software Engineer who is passionate about building the future. He also loves fluffy animals!</div>
+            <img className="about-img" src={kiwi}/>
+            <h2>Software Engineer & Animal Lover</h2>
+            <div className="about-desc">Benjamin attended a coding bootcamp called App Academy. He has made many friends and learned how to think like a programmer. He now views the world through a whole new lens!</div>
+        
+            <div className="testimonials">
+                <div className="t-name"></div>
+                <div className="t-t"></div>
+                <div className="t-name"></div>
+                <div className="t-t"></div>
+                <div className="t-name"></div>
+                <div className="t-t"></div>
+
+            </div>
         </div>
     )
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,59 +1,91 @@
-import { NavLink} from "react-router-dom";
-import {Link} from "react-scroll";
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { Link } from "react-scroll";
 import "./navbar.css";
-import "./style.css"
+import "./style.css";
 
 function Nav() {
+  // // to change burger classes
+  // const [burger_class, setBurgerClass] = useState("burger-bar unclicked");
+  // const [menu_class, setMenuClass] = useState("menu hidden");
+  // const [isMenuClicked, setIsMenuClicked] = useState(false);
+
+  // // toggle burger menu change
+  // const updateMenu = () => {
+  //   if (!isMenuClicked) {
+  //     setBurgerClass("burger-bar clicked");
+  //     setMenuClass("menu visible");
+  //   } else {
+  //     setBurgerClass("burger-bar unclicked");
+  //     setMenuClass("menu hidden");
+  //   }
+  //   setIsMenuClicked(!isMenuClicked);
+  // };
 
   return (
     <nav>
       <Link to="/" className="logoname">
-        Benjamin Lau
+        Benjamin
       </Link>
+      {/* <div className="dropdown">
+        <div className="dropbtn" onClick={updateMenu}>Dropdown</div>
+        <div className="dropdown-content"> */}
+          {/* <Link
+            to="/"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="navigation"
+            activeClass="navigation-active"
+          >
+            Main
+          </Link> */}
 
-      <Link
-        to="/"
-        spy={true}
-        smooth={true}
-        duration={500}
-        className="navigation"
-        activeClass="navigation-active"
-      >
-        Main
-      </Link>
+          <Link
+            to="about"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="navigation"
+            activeClass="navigation-active"
+          >
+            About
+          </Link>
 
-      <Link
-        to="about"
-        spy={true}
-        smooth={true}
-        duration={500}
-        className="navigation"
-        activeClass="navigation-active"
-      >
-        About
-      </Link>
+          <Link
+            to="resume"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="navigation"
+            activeClass="navigation-active"
+          >
+            Resume
+          </Link>
 
-      <Link
-        to="resume"
-        spy={true}
-        smooth={true}
-        duration={500}
-        className="navigation"
-        activeClass="navigation-active"
-      >
-        Resume
-      </Link>
+          <Link
+            to="skills"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="navigation"
+            activeClass="navigation-active"
+          >
+            Skills
+          </Link>
 
-      <Link
-        to="skills"
-        spy={true}
-        smooth={true}
-        duration={500}
-        className="navigation"
-        activeClass="navigation-active"
-      >
-        Skills
-      </Link>
+          <Link
+            to="contact"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="navigation"
+            activeClass="navigation-active"
+          >
+            Contact
+          </Link>
+        {/* </div>
+      </div> */}
 
       {/* <NavLink to="/">Main</NavLink>
       <NavLink to="/about">About</NavLink>
