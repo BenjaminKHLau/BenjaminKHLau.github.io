ac0d8ce07502a8a38626d9945a39910ca64139fa
Ben <benjaminkhlau@gmail.com>
2023-04-07 02:38:31 -0500

    testing pdf resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,4 +1,4 @@
-
+import resume from "../pics/01BenjaminLauResumeREAL.pdf"
 
 
 
@@ -7,7 +7,12 @@ function Resume(){
 
     return (
         <div id="resume">
-        Resume section is a work in progress
+        {/* Resume section is a work in progress */}
+
+
+        <object data={resume} type="application/pdf" width="100%" height="100%">
+      <p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf">to the PDF!</a></p>
+  </object>
         </div>
     )
 }
