449605f0fd01be7ed125694bb66c4eb946aedd7b
Ben <benjaminkhlau@gmail.com>
2023-04-23 23:49:29 -0500

    deleted the alt text to make code look better

--- src/components/resume.js
+++ src/components/resume.js
@@ -10,7 +10,6 @@ function Resume(){
 
 
         <object data={resume} type="application/pdf" width="70%" height="100%">
-      {/* <p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf">to the PDF!</a></p> */}
   </object>
 
 {/* <Document file={resume}>
